f1d9900ea664eeed27e23504e1dc8d6cc7bd41cd
zahmis <meda829ratteru86@gmail.com>
2023-08-09 10:25:54 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+func main() {
+	// UUIDの文字列表現
+	originalUUID, err := uuid.NewRandom()
+	if err != nil {
+		fmt.Println("uuid 生成失敗", err)
+		return
+	}
+
+	// UUIDを16バイトのバイナリ表現に変換
+	uuidObj, err := uuid.Parse(originalUUID.String())
+	if err != nil {
+		fmt.Println("バイナリ変換に失敗", err)
+		return
+	}
+
+	// Base64エンコード
+	encoded := base64.RawURLEncoding.EncodeToString(uuidObj[:])
+
+	// Base64デコード
+	decoded, err := base64.RawURLEncoding.DecodeString(encoded)
+	if err != nil {
+		fmt.Println("デコード失敗", err)
+		return
+	}
+
+	// バイナリ表現を文字列表現に変換
+	decodedUUID, err := uuid.FromBytes(decoded)
+	if err != nil {
+		fmt.Println("文字列置換失敗", err)
+		return
+	}
+
+	// 結果
+	fmt.Println("オリジナル:", originalUUID)
+	fmt.Println("エンコード:", encoded)
+	fmt.Println("デコード:", decodedUUID.String())
+}
